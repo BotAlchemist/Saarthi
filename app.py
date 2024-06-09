13c1e3f06bef92476228416727108207a4627e91
Sumit Srivastava <sumvast@gmail.com>
2024-06-09 18:11:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-
-
 import streamlit as st  #Web App
 from PIL import Image #Image Processing
 import numpy as np #Image Processing
@@ -13,50 +11,22 @@ from urllib.parse import urlparse
 st.set_page_config(layout="wide")
 
 i_openai_key = st.sidebar.text_input("OpenAI key", type='password')
+
+
 if len(i_openai_key) > 0:
+
+    #----------------- Global variables -------------------------
+    image_folder_path= 'sample_data/'
     os.environ["OPENAI_API_KEY"]= i_openai_key
     client = OpenAI()
-    
-    
-    #----------------- GLobal variables -------------------------
-    image_folder_path= 'sample_data/'
     #------------------------------------------------------------
-    
+
     def encode_image(image_path):
         with open(image_path, "rb") as image_file:
             return base64.b64encode(image_file.read()).decode('utf-8')
-    
-    
-    i_input_type= st.selectbox('Choose task:', ["Open camera", "Upload image"])
-    
-    if i_input_type == "Open camera": 
-        i_prompt_template= st.selectbox("Choose prompt", ["Answer MCQ", "None"])
-        if i_prompt_template == "Answer MCQ":
-            i_mcq_type= st.radio("Choose MCQ type", ["Single", "Multiple"], horizontal=True)
-    
-            i_user_prompt= '''Extract all text from the images. The image contains text in the form of multiple choice questions.
-             Ignore any watermarks. Format the output in the form of multiple choice questions. 
-             provide suitable line breaks with numbering if any.'''
-    
-            if i_mcq_type == "Single":
-                i_user_prompt+= "\n Each question can have only one answer."
-            elif i_mcq_type == "Multiple":
-                i_user_prompt+= "\n Each question can have multiple answers."
-            
-        elif i_prompt_template == "None":
-            i_user_prompt= st.text_area("Type your prompt", "")
-        
-        image_local = st.camera_input("Take a picture")
-        if image_local and len(i_prompt_template)>5:
-            with open(os.path.join(image_folder_path,"test.jpg"),"wb") as f:
-                f.write(image_local.getbuffer())
-    
-            image_local_temp = os.path.join(image_folder_path,"test.jpg")
-            image_url = f"data:image/jpeg;base64,{encode_image(image_local_temp)}"
-    
-            
-    
-            response = client.chat.completions.create(
+
+    def extract_text_from_image(i_user_prompt, image_url ):
+        response = client.chat.completions.create(
               model='gpt-4-vision-preview',
               messages=[
                   {
@@ -72,25 +42,114 @@ if len(i_openai_key) > 0:
               ],
               max_tokens=1000,
           )
-            
-            ocr_string = response.choices[0].message.content
-            st.write(ocr_string)
-    
-            i_user_prompt_final= '''Provide the correct answern for below multiple choice question.
-              First answer what the correct answern is and then explain why you chose this in 2 to 3 lines. \n''' + ocr_string
-    
-            response= client.chat.completions.create(
+        return response.choices[0].message.content
+
+    def get_gpt_response(i_user_prompt_final, i_temperature):
+        response= client.chat.completions.create(
               model="gpt-3.5-turbo",
               messages=[
                   {"role": "user", "content": i_user_prompt_final}
-              ]
+              ],
+              temperature=i_temperature
           )
+        return response.choices[0].message.content, response.usage.total_tokens
+
+
+    i_menu= st.sidebar.selectbox("Menu", ['Chat', 'Vision', 'Co-pilot'])
+
+#------------------------------------- Use GTP chat ---------------------------------------------    
+    if i_menu== 'Chat':
+        i_chat_prompt= st.text_area(":writing_hand:", placeholder="Type your prompt", height=200, key='chat_key')
+        i_temperature = st.slider(":thermometer:", min_value = 0.0, max_value = 2.0, value= 0.3, step=0.1)
+        got_response= False
+        if st.button("Ask") and len(i_chat_prompt)>5:
             st.divider()
-            llm_output= response.choices[0].message.content
-            llm_tokens= response.usage.total_tokens
+            llm_output, llm_tokens= get_gpt_response(i_chat_prompt, i_temperature)
+            got_response= True
+        
+        if got_response:
             st.write(llm_output)
+            st.divider()
             st.metric(label="Tokens", value=llm_tokens)
+    
+
+#------------------------------------- Use GTP vision ---------------------------------------------
+    elif i_menu== 'Vision':   
+        i_input_type= st.selectbox('Choose task:', ["Open camera", "Upload image"])
+
+        if i_input_type == "Open camera":
+            i_prompt_template= st.selectbox("Choose prompt", ["Answer","Label", "None"])
+            if i_prompt_template == "Answer":
+                i_mcq_type= st.radio("Choose MCQ type", ["Single", "Multiple"], horizontal=True)
+
+                i_user_prompt= '''Extract all text from the images. The image contains text in the form of multiple choice questions.
+                Ignore any watermarks. Format the output in the form of multiple choice questions.
+                provide suitable line breaks with numbering if any.'''
+
+                if i_mcq_type == "Single":
+                    i_user_prompt+= "\n Each question can have only one answer."
+                elif i_mcq_type == "Multiple":
+                    i_user_prompt+= "\n Each question can have multiple answers."
+
+            elif i_prompt_template == "None":
+                i_user_prompt= st.text_area("Type your prompt", "")
+
+            elif i_prompt_template == "Label":
+                i_user_prompt= '''You are provided with a image of product with its labels and ingredients list.
+                Your goal is to extract all texts from labels, ingredients or nutrition facts..
+               
+                If the image doesnot contain any labels or ingredients, provide a response: "Image does not contain necessary details".
+                '''
+
+            image_local = st.camera_input("Take a picture")
+            if image_local and len(i_user_prompt)>5:
+                with open(os.path.join(image_folder_path,"test.jpg"),"wb") as f:
+                    f.write(image_local.getbuffer())
+
+                image_local_temp = os.path.join(image_folder_path,"test.jpg")
+                image_url = f"data:image/jpeg;base64,{encode_image(image_local_temp)}"
+
+                #st.write(i_user_prompt)
+                ocr_string = extract_text_from_image(i_user_prompt, image_url )
+                st.write(ocr_string)
+
+                if i_prompt_template == "Answer":
+                    i_user_prompt_final= '''Provide the correct answern for below multiple choice question.
+                      First answer what the correct answern is and then explain why you chose this in 2 to 3 lines. \n''' + ocr_string
+                    st.divider()
+                    llm_output, llm_tokens= get_gpt_response(i_user_prompt_final, 0.2)
+                    st.write(llm_output)
+                    st.metric(label="Tokens", value=llm_tokens)
+
+                elif i_prompt_template == "Label":
+                    i_user_prompt_final=''' You are an expert nutritionist.
+                    Carefully analyze  all the labels and ingredients.
+                    Provide the list of ingredients along with a line of what the ingredient is.
+                    Provide a brief summary of the extracted label information, highlighting the key details.
+                    Focus more on the additives, preservatives, artificial colors or flavors or any harmful substances present.
+                    ''' + ocr_string
+
+                    st.divider()
+                    llm_output, llm_tokens= get_gpt_response(i_user_prompt_final, 0.3)
+                    st.write(llm_output)
+                    st.metric(label="Tokens", value=llm_tokens)
+
+                    
+
+
+
 
 else:
-    st.warning("Please enter your OpenAI key")
-
+    st.info("Please enter your OpenAI key")
+    
+    st.code("sk-proj-7uK5yZ4zEeXyPbrMPJf3sdOrpVHgyEsAHGig94MGVzW1AxdRXF")
+    st.code("sk-proj-nugHpvIH1whBPpcEVLnktMHfQTNh7n2muDQRrM5wd6DTNsYlJz")
+    st.code("sk-proj-aVA9zex4cECRIU1kIqZWT3BlbkFJF2wZ0WEuG7tpemSfxubn")
+    st.code("sk-proj-c4uc7o2F5VGsSYgc1PfUgDtAE6KNC8iMrJRZKVz32Kh0N1Olb3")
+    st.code("sk-proj-ITf7c0lWVCeNi2DPU3YWobQTAn6evVQlnN9Z7f8pDquTQuVhv")
+    st.code("sk-proj-nugHpvIH1whBPpcEVLnktMHfQTNh7n2muDQRrM5wd6DTNsYlJj")
+    st.code("sk-proj-5lNHypFjNexYEkqjNawyXRl0dlR8FNiVjd6GxoLyAtan5ZtXx")
+    st.code("sk-proj-S4svUFupfUHlH5XRU6nbCuwKuS5E8fhka8Ub3EfkpW7d5QZn")
+    st.code("sk-proj-7uK5yZ4zEeXyPbrMPJf3sdOrpVHgyEsAHGig94MGVzW1Axdr")
+    st.code("sk-proj-aVA7zex4cFCRIU0kIqZWT3BlbkFJF2wZ0WEuG7thenYfcubn")
+    st.code("sk-proj-FIfvIkWdKghp9qaCR7XlLU9EoMu6iYjoSeDVtL3BRtO7pUbo")
